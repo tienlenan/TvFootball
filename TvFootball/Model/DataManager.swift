92138f6fec74ca04d29653bb06e9a74ddfd3a34a
Tien An <an.letien@toshiba-tsdv.com>
2018-08-08 17:46:45 +0700

    Update get matches method

--- TvFootball/Model/DataManager.swift
+++ TvFootball/Model/DataManager.swift
@@ -12,23 +12,37 @@ import SwiftyJSON
 
 class DataManager: NSObject {
     static let shared: DataManager = DataManager()
+    weak var delegate: HTTPDelegate?
+    var liveMatches: [LiveMatch] = []
     
+    /// Get live matches
     func getLiveMatches() {
+        self.liveMatches = []
         let requestURL = "http://api.bongdahd.info/api/fixture/list"
         Alamofire.request(requestURL, encoding: JSONEncoding.default)
             .responseJSON { response in
                 debugPrint(response)
-                if let httpError = response.result.error {
+                if let _ = response.result.error {
                     // Connection error
                     print("Connection error...")
+                    self.delegate?.didGetConnectionError(message: "Connection error.")
                 } else { // No errors
                     let responseJSONData = JSON(response.result.value!)
                     let statusCode = (response.response?.statusCode)
                     if statusCode == 200 {
-                        print("test: \(responseJSONData)")
+                        print("Live Data: \(responseJSONData)")
+                        let matchesDataJSON = responseJSONData["result"]
+                        for i in 0..<matchesDataJSON.count {
+                            let match = LiveMatch.init(jsonData: matchesDataJSON[i])
+                            self.liveMatches.append(match)
+                        }
+                        print("Length: \(self.liveMatches.count)")
+                        
+                        self.delegate?.didGetSuccessRespond(data: responseJSONData)
                     } else {
                         // Error from server
                         print("Error from server...")
+                        self.delegate?.didGetErrorFromServer(message: "Error from server.")
                     }
                 }
         }
