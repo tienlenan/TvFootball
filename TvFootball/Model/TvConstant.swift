7afc495bd69a6381b2d73c4de4fdd537060bf6b9
Le Tien An <admin@macbook-air.local>
2018-08-30 22:01:52 +0700

    Update source

--- TvFootball/Model/TvConstant.swift
+++ TvFootball/Model/TvConstant.swift
@@ -13,7 +13,7 @@ class TvConstant {
     static let BANNER_IMAGE_URL = "https://i.imgur.com/QZ6j8Mg.png"
     
     /// Ads web url
-    static let ADS_URL = "http://event.w88tip.com/"
+    static let ADS_URL = "http://nghienbongda.net/iframe/index.php"
     
     /// For getting live matches url
     static let GET_LIVE_MATCHES_API_URL = "http://api.bongdahd.info/api/fixture/list"
